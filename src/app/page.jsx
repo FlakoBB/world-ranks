583a5fd7295d5fc6ee44be99c19d1dec6531e41a
Joseph Ryan <bbjosephryan@gmail.com>
2024-12-10 15:21:37 -0600

    add: logo and header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
+import styles from '@/styles/mainPage.module.scss'
+
 export default function Home () {
   return (
-    <h1>World Ranks</h1>
+    <main className={styles.main}>
+      <div className={styles.logo}>
+        <img src='/Logo.svg' alt='World Ranks logo' />
+      </div>
+      <div className={styles.content}>
+        <header className={styles.header}>
+          <p className={styles.countries_found}>Found 234 countries</p>
+          <div className={styles.search_bar}>
+            <button>
+              <img src='/Search.svg' alt='Magnifying glass icon' />
+            </button>
+            <input type='search' name='search' id='search' placeholder='Search by Name, Region, Subregion' />
+          </div>
+        </header>
+      </div>
+    </main>
   )
 }
