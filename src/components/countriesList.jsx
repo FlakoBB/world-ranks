18c6853c1152df40f7c1aadb6d69397bd414b645
Joseph Ryan <bbjosephryan@gmail.com>
2024-12-12 15:58:54 -0600

    feat: list pagination | list sorted by population

--- src/components/countriesList.jsx
+++ src/components/countriesList.jsx
@@ -3,22 +3,19 @@ import { useTransitionRouter } from 'next-view-transitions'
 import styles from '@/styles/countriesList.module.scss'
 
 const CountriesList = ({ countries = [] }) => {
-  // const [countriesPerPage, setCountriesPerPage] = useState(30)
   const countriesPerPage = 30
+  const lastPage = Math.ceil(countries.length / countriesPerPage)
   const [currentPage, setCurrentPage] = useState(1)
-  const [startIndex, setStartIndex] = useState((currentPage - 1) * countriesPerPage)
-  const [endIndex, setEndIndex] = useState(startIndex + countriesPerPage)
-  const [currentCountries, setCurrentCountries] = useState([...countries.slice(startIndex, endIndex)])
+  const [currentCountries, setCurrentCountries] = useState([])
 
   const router = useTransitionRouter()
 
   useEffect(() => {
-    const newStartIndex = (currentPage - 1) * countriesPerPage
-    const newEndIndex = newStartIndex + countriesPerPage
-    setStartIndex(newStartIndex)
-    setEndIndex(newEndIndex)
-    setCurrentCountries([...countries.slice(newStartIndex, newEndIndex)])
-  }, [currentPage])
+    const countriesSorted = countries.sort((a, b) => b.population - a.population)
+    const startIndex = (currentPage - 1) * countriesPerPage
+    const endIndex = startIndex + countriesPerPage
+    setCurrentCountries([...countriesSorted.slice(startIndex, endIndex)])
+  }, [currentPage, countries])
 
   const prevPage = () => {
     setCurrentPage(prev => {
@@ -32,12 +29,16 @@ const CountriesList = ({ countries = [] }) => {
 
   const nextPage = () => {
     setCurrentPage(prev => {
-      if (prev < Math.ceil(countries.length / countriesPerPage)) {
+      if (prev < lastPage) {
         return prev + 1
       } else {
         return prev
       }
     })
+  }
+
+  const goToPage = (pageNumber) => {
+    setCurrentPage(pageNumber)
   }
 
   return (
@@ -48,7 +49,7 @@ const CountriesList = ({ countries = [] }) => {
             <th>Flag</th>
             <th>Name</th>
             <th>Population</th>
-            <th>Area (km2)</th>
+            <th>Area (km<sup>2</sup>)</th>
             <th>Region</th>
           </tr>
         </thead>
@@ -62,18 +63,34 @@ const CountriesList = ({ countries = [] }) => {
                   </figure>
                 </td>
                 <td className={styles.text}>{country.name.common}</td>
-                <td className={styles.text}>{country.population}</td>
-                <td className={styles.text}>{country.area}</td>
+                <td className={styles.text}>{country.population.toLocaleString('en-US')}</td>
+                <td className={styles.text}>{country.area.toLocaleString('en-US')}</td>
                 <td className={styles.text}>{country.region}</td>
               </tr>
             ))
           }
         </tbody>
       </table>
-      <div>
-        <button onClick={prevPage}>previous</button>
-        <p>{currentPage}</p>
-        <button onClick={nextPage}>next</button>
+      <div className={styles.pagination_bar}>
+        <button className={styles.previous_btn} onClick={prevPage}>
+          <svg width='16' height='16' viewBox='0 0 16 16' fill='none'>
+            <path d='M12 6L8 10L4 6' stroke='#6C727F' strokeWidth='2' />
+          </svg>
+        </button>
+        <ul className={styles.pages_list}>
+          {
+            Array.from({ length: Math.ceil(countries.length / countriesPerPage) }).map((_, index) => (
+              <li key={index}>
+                <button className={`${styles.page_btn} ${currentPage === index + 1 && styles.current}`} onClick={() => goToPage(index + 1)}>{index + 1}</button>
+              </li>
+            ))
+          }
+        </ul>
+        <button className={styles.next_btn} onClick={nextPage}>
+          <svg width='16' height='16' viewBox='0 0 16 16' fill='none'>
+            <path d='M12 6L8 10L4 6' stroke='#6C727F' strokeWidth='2' />
+          </svg>
+        </button>
       </div>
     </div>
   )
